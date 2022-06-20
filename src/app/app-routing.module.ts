9929935ba20095e6e7f35163694d4f810b01f6ae
Dario Lucić <dlucic@it4.ltd>
2022-06-20 15:49:24 +0200

    added app pages and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PosComponent } from './pages/pos/pos.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'home', component: TransactionsComponent },
+  { path: 'pos', component: PosComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { LogoutComponent } from './auth/logout/logout.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { PosComponent } from './pages/pos/pos.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LogoutComponent
+    LogoutComponent,
+    RegistrationComponent,
+    TransactionsComponent,
+    PosComponent,
+    ProductsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
