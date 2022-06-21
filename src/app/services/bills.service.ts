1f7101a75984e70ae215a5ec099c299eb98d51e9
Dario Lucić <dlucic@it4.ltd>
2022-06-21 15:30:18 +0200

    added methods to bills service

--- src/app/services/bills.service.ts
+++ src/app/services/bills.service.ts
@@ -1,9 +1,59 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BillBody } from '../models/bill-body.model';
+import { BillHeader } from '../models/bill-header.model';
+import { BaseApiService } from './base-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BillsService {
+export class BillsService extends BaseApiService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http, 'api/bill');
+  }
+
+  headerPath: string = 'header/';
+  bodyPath: string = 'body/'
+
+  getAllBillHeaders(): Observable<Object> {
+    return this.http.get(this.apiRoute + this.headerPath);
+  }
+
+  getBillHeaderByID(id: number): Observable<Object> {
+    return this.http.get(this.apiRoute + this.headerPath + id);
+  }
+
+  addNewHeader(header: BillHeader): Observable<Object> {
+    return this.http.post(this.apiRoute + this.headerPath, header);
+  }
+
+  updateHeader(header: BillHeader): Observable<Object> {
+    return this.http.put(this.apiRoute, header);
+  }
+
+  deleteHeader(id: number): Observable<Object> {
+    return this.http.delete(this.apiRoute + this.headerPath + id);
+  }
+
+  getAllBillBodies(): Observable<Object> {
+    return this.http.get(this.apiRoute + this.bodyPath);
+  }
+
+  getBillBodyByID(id: number): Observable<Object> {
+    return this.http.get(this.apiRoute + this.bodyPath + id);
+  }
+
+  addNewBody(body: BillBody): Observable<Object> {
+    return this.http.post(this.apiRoute + this.bodyPath, body);
+  }
+
+  updateBody(body: BillBody): Observable<Object> {
+    return this.http.put(this.apiRoute, body);
+  }
+
+  deleteBody(id: number): Observable<Object> {
+    return this.http.delete(this.apiRoute + this.bodyPath + id);
+  }
 }
