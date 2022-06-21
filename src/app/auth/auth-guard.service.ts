884772f3eb5f93cb13d688d5f337f98c5fd6c8ef
Dario Lucić <dlucic@it4.ltd>
2022-06-21 12:36:21 +0200

    tested auth-guard with true return values

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, UrlTree, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return true;
+  }
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return true;
+  }
 }
