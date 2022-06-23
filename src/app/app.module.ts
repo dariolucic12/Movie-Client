568afad9e896b1cdce4f0fdafc32a779975798da
Ranko Koturić <rkoturic@it4.ltd>
2022-06-23 15:58:43 +0200

    Interceptor add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagesModule } from './pages/pages.module';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import { AuthModule } from './auth/auth.module';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,14 @@ import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
     MatToolbarModule,
     AuthModule
   ],
-  providers: [JwtHelperService, {provide: JWT_OPTIONS, useValue: JWT_OPTIONS}],
+  providers: [JwtHelperService, 
+  { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
