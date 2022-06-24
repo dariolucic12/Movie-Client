f6434061a8245ebc7699e834921834249e04c100
Ranko Koturić <rkoturic@it4.ltd>
2022-06-24 08:18:58 +0200

    Auth guard implemented

---
+++ src/app/guards/loged-in.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginComponent } from '../auth/login/login.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogedInGuard implements CanActivate {
+
+  constructor(private loginAuth: LoginComponent, private router: Router) {
+
+  }
+  canActivate(){
+    if(!this.loginAuth.isAuthenticated()){
+      this.router.navigate(['/pages/home'])
+    }
+    return true;
+  }
+  
+}
