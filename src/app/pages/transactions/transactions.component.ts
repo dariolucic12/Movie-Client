07c83cb0a46ebce9edcc8a14dc5cb9d52d6cf25d
Dario Lucić <dlucic@it4.ltd>
2022-06-24 15:20:55 +0200

    added getTransactions from db method

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { BillBody } from 'src/app/models/bill-body.model';
 import { Product } from 'src/app/models/product.model';
 import { BillsService } from 'src/app/services/bills.service';
 
-const BILL_DATA: BillBody[] = [
-  { price: 2, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12},
-  { price: 23.00, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12},
-  { price: 23.00, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12},
-  { price: 23.00, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12}
-];
+// const BILL_DATA: BillBody[] = [
+//   { price: 2, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12 },
+//   { price: 23.00, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12 },
+//   { price: 23.00, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12 },
+//   { price: 23.00, quantity: 59, discount: 17, discountAmount: 22, totalPrice: 23, productId: 3, billHeaderId: 12 }
+// ];
 
 @Component({
   selector: 'app-transactions',
@@ -18,17 +18,20 @@ const BILL_DATA: BillBody[] = [
 export class TransactionsComponent implements OnInit {
 
   displayedColumns: string[] = ['price', 'quantity', 'discount', 'discountAmount', 'totalPrice', 'productId', 'billHeaderId'];
-  dataSource = BILL_DATA;
+  dataSource: BillBody[] = [];
 
-  constructor(private billsService: BillsService) { }
+  constructor(private billsService: BillsService, private changeDetectorRefs: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    // this.billsService.getAllBillBodies().subscribe(
-    //   response => {
-    //     console.log(response);
-    //   }
-    // );
-    
+    this.billsService.getAllBillBodies().subscribe((data: any) => {
+      this.dataSource = data;
+      console.log(this.dataSource);
+      this.changeDetectorRefs.detectChanges();
+
+      return data;
+    }
+    );
+
     // this.billsService.getBillBodyByID(5).subscribe(
     //   response => {
     //     console.log(response);
