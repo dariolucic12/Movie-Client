b91d869b1c602df6a3cacf8fc41983c5db9aa059
Dario Lucić <dlucic@it4.ltd>
2022-06-29 14:57:58 +0200

    all add-edit logic in the same component

--- src/app/pages/products/add-edit-product/add-edit-product/add-edit-product.component.ts
+++ src/app/pages/products/add-edit-product/add-edit-product/add-edit-product.component.ts
@@ -1,9 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Product } from 'src/app/models/product.model';
-import { NgModel } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-product',
