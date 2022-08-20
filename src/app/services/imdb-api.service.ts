30907e3938fe71bf2968c7fb23e68780f2f79a86
Dario Lucić <dlucic@it4.ltd>
2022-08-20 16:31:27 +0200

    napravljen ImdbApiService servis

---
+++ src/app/services/imdb-api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Top250Movie } from '../models/top250Movie.model';
+import { BaseApiService } from './base-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImdbApiService extends BaseApiService {
+
+  constructor(http: HttpClient) {
+    super(http, 'https://imdb-api.com/en/API/Top250Movies/k_6x2wbf33');
+  }
+
+  getTopMovies(): Observable<Top250Movie[]> {
+    return this.http.get(this.apiRoute).pipe(
+      map(response => response as Top250Movie[])
+    );
+  }
+}
