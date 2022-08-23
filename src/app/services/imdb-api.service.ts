15d37aeefc3bfba4f4cd530da67c718600b111a6
Dario Lucić <dlucic@it4.ltd>
2022-08-23 15:43:24 +0200

    #2 dodana getMovieById metoda u servis

--- src/app/services/imdb-api.service.ts
+++ src/app/services/imdb-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { Movie } from '../models/movie';
 import { Top250Movie } from '../models/top250Movie.model';
 import { BaseApiService } from './base-api.service';
 
@@ -16,6 +17,12 @@ export class ImdbApiService extends BaseApiService {
   getTopMovies(): Observable<Top250Movie[]> {
     return this.http.get(this.apiRoute).pipe(
       map(response => response as Top250Movie[])
+    );
+  }
+
+  getMovieById(id: string): Observable<Movie>{
+    return this.http.get("https://imdb-api.com/en/API/Title/k_6x2wbf33/" + id).pipe(
+      map(response => response as Movie)
     );
   }
 }
