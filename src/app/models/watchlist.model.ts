4fb60d3088c876fa3ad66bd192444c8f2091b8d2
Dario Lucić <dlucic@it4.ltd>
2022-08-24 14:21:48 +0200

    #3 dodan Watchlist model

---
+++ src/app/models/watchlist.model.ts
@@ -0,0 +1,9 @@
+export interface Watchlist {
+    id?: number;
+    userID: string;
+    movieId: string;
+    fullTitle: string;
+    image: string;
+    imDbRating: string;
+    imDbRatingCount: string;
+}
